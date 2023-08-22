187183727921dd69cd4dbd88918490e63ce66426
jotertu <joaotertuliano00@gmail.com>
2023-08-22 14:19:47 -0300

    Adicionado comentários ao arquivo bancoEscola.

--- Atividades 01/bancoDeDadosEscola.sql
+++ Atividades 01/bancoDeDadosEscola.sql
@@ -1,9 +1,15 @@
 DROP DATABASE IF EXISTS db_Escola;
 
+/*
+1 -  Crie um banco de dados para um registro de uma escola, onde o sistema trabalhará com as informações dos estudantes deste registro dessa escola.
+*/
 CREATE DATABASE IF NOT EXISTS db_Escola;
 
 USE db_Escola;
 
+/*
+2 - Crie uma tabela estudantes e utilizando a habilidade de abstração e determine 5 atributos relevantes dos estudantes para se trabalhar com o serviço dessa escola.
+*/
 CREATE TABLE IF NOT EXISTS tb_Alunos (
     id_aluno BIGINT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
@@ -22,6 +28,9 @@ CREATE TABLE IF NOT EXISTS tb_Alunos (
 
 SHOW TABLES;
 
+/*
+3 - Insira nesta tabela no mínimo 8 dados (registros).
+*/
 INSERT INTO tb_Alunos (
     nome,
     email,
@@ -137,12 +146,88 @@ INSERT INTO tb_Alunos (
       '1'
 );
 
+INSERT INTO tb_Alunos (
+    nome,
+    email,
+    nascimento,
+    sexo,
+    endereco,
+    telefone,
+    nota,
+    turma,
+    frequencia
+) VALUES (
+    'Carla da Silva',
+    'carla.silva@email.com',
+    '1995-09-15',
+    'f',
+    'Rua das Flores, 123 - Centro - São Paulo - SP',
+    '(11)9876-5432',
+    7.5,
+    't1',
+    '1'
+);
+
+INSERT INTO tb_Alunos (
+    nome,
+    email,
+    nascimento,
+    sexo,
+    endereco,
+    telefone,
+    nota,
+    turma,
+    frequencia
+) VALUES (
+    'André Santos',
+    'andre.santos@email.com',
+    '1992-06-08',
+    'm',
+    'Rua dos Pinheiros, 456 - Vila Nova - Rio de Janeiro - RJ',
+    '(21)5555-7890',
+    9.0,
+    't3',
+    '1'
+);
+
+INSERT INTO tb_Alunos (
+    nome,
+    email,
+    nascimento,
+    sexo,
+    endereco,
+    telefone,
+    nota,
+    turma,
+    frequencia
+) VALUES (
+    'Mariana Oliveira',
+    'mariana.oliveira@email.com',
+    '1998-12-02',
+    'f',
+    'Av. Paulista, 789 - Bela Vista - São Paulo - SP',
+    '(11)6543-2109',
+    6.8,
+    't2',
+    '1'
+);
+
+
 SELECT * FROM tb_Alunos;
 
+/*
+4 -  Faça um SELECT que retorne todes o/a(s) estudantes com a nota maior do que 7.0.
+*/
 SELECT * FROM tb_Usuarios WHERE nota > 7.0;
 
+/*
+5 -  Faça um SELECT que retorne todes o/a(s) estudantes com a nota menor do que 7.0.
+*/
 SELECT * FROM tb_Usuarios WHERE nota < 7.0;
 
+/*
+6 - Ao término atualize um registro desta tabela através de uma query de atualização.
+*/
 UPDATE tb_Alunos SET nome = 'Tiago Pietro Corte Real Fernando', nota = 5.4 WHERE id_Aluno = 4; 
 
 SELECT * FROM tb_Alunos;
